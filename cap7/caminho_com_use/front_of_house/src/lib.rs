5bbf2fbe352c09d304d6773f6b4de74ac6a5756c
MunrraMT <dev.armsantos@gmail.com>
2024-07-02 17:40:50 -0400

    ch07-04-bringing-paths-into-scope-with-the-use-keyword

---
+++ cap7/caminho_com_use/front_of_house/src/lib.rs
@@ -0,0 +1,33 @@
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// use crate::front_of_house::hosting;
+
+// pub fn eat_at_restaurant() {
+//     hosting::add_to_waitlist();
+// }
+
+// ------------------------------------
+
+// use std::collections::HashMap;
+
+// fn main() {
+//     let mut map = HashMap::new();
+//     map.insert(1, 2);
+// }
+
+// ------------------------------------
+
+// use std::fmt::Result;
+// use std::io::Result as IoResult;
+
+// fn function1() -> Result {
+//     // --snip--
+// }
+
+// fn function2() -> IoResult<()> {
+//     // --snip--
+// }
