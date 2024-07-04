17602df2f066212b73be9927abbaf77cc4d7c1d2
MunrraMT <dev.armsantos@gmail.com>
2024-07-04 10:14:11 -0400

    ch10-00-generics

---
+++ cap10/current/src/main.rs
@@ -0,0 +1,71 @@
+fn main() {
+    maior_numero_1_lista();
+    println!("--------");
+    maior_numero_2_listas();
+    println!("--------");
+    maior_numero_com_abstracao();
+}
+
+fn maior_numero_1_lista() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = &number_list[0];
+
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {largest}");
+}
+
+fn maior_numero_2_listas() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = &number_list[0];
+
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {largest}");
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let mut largest = &number_list[0];
+
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {largest}");
+}
+
+fn maior_numero_com_abstracao() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {result}");
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("The largest number is {result}");
+
+    fn largest(list: &[i32]) -> &i32 {
+        let mut largest = &list[0];
+
+        for item in list {
+            if item > largest {
+                largest = item;
+            }
+        }
+
+        largest
+    }
+}
