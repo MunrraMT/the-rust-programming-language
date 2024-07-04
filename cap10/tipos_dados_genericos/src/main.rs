33ace3031500756a9bc0da63d0fa0ad83026a7a1
MunrraMT <dev.armsantos@gmail.com>
2024-07-04 14:28:12 -0400

    ch10-01-syntax

---
+++ cap10/tipos_dados_genericos/src/main.rs
@@ -0,0 +1,156 @@
+use std::cmp;
+
+fn main() {
+    definindo_funcoes();
+    println!("---------------");
+    definindo_funcoes_com_genericos();
+    println!("---------------");
+    definindo_struct_com_unico_tipo();
+    println!("---------------");
+    definindo_struct_com_multiplos_tipos();
+    println!("---------------");
+    definindo_enum();
+    println!("---------------");
+    definindo_metodos();
+    println!("---------------");
+    definindo_metodos_com_parametros_genericos();
+}
+
+fn definindo_funcoes() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {result}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest number is {result}");
+
+    fn largest_i32(list: &[i32]) -> &i32 {
+        let mut largest = &list[0];
+
+        for item in list {
+            if item > largest {
+                largest = item;
+            }
+        }
+
+        largest
+    }
+
+    fn largest_char(list: &[char]) -> &char {
+        let mut largest = &list[0];
+
+        for item in list {
+            if item > largest {
+                largest = item;
+            }
+        }
+
+        largest
+    }
+}
+
+fn definindo_funcoes_com_genericos() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {result}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest number is {result}");
+
+    fn largest<T: cmp::PartialOrd>(list: &[T]) -> &T {
+        let mut largest = &list[0];
+
+        for item in list {
+            if item > largest {
+                largest = item;
+            }
+        }
+
+        largest
+    }
+}
+
+fn definindo_struct_com_unico_tipo() {
+    struct Point<T> {
+        x: T,
+        y: T,
+    }
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+}
+
+fn definindo_struct_com_multiplos_tipos() {
+    struct Point<T, U> {
+        x: T,
+        y: U,
+    }
+
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+}
+
+fn definindo_enum() {
+    enum Option<T> {
+        Some(T),
+        None,
+    }
+
+    enum Result<T, E> {
+        Ok(T),
+        Err(E),
+    }
+}
+
+fn definindo_metodos() {
+    struct Point<T> {
+        x: T,
+        y: T,
+    }
+
+    impl<T> Point<T> {
+        fn x(&self) -> &T {
+            &self.x
+        }
+    }
+
+    impl Point<f32> {
+        fn distance_from_origin(&self) -> f32 {
+            (self.x.powi(2) + self.y.powi(2)).sqrt()
+        }
+    }
+
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {}", p.x());
+    println!("{}", 0.0012 + 0.0015);
+}
+
+fn definindo_metodos_com_parametros_genericos() {
+    struct Point<X1, Y1> {
+        x: X1,
+        y: Y1,
+    }
+
+    impl<X1, Y1> Point<X1, Y1> {
+        fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+            Point {
+                x: self.x,
+                y: other.y,
+            }
+        }
+    }
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
