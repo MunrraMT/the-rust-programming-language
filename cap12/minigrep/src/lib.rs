982469748dab200b105ee263998f926b4cc53f19
MunrraMT <dev.armsantos@gmail.com>
2024-07-08 09:54:46 -0400

    ch12-04-testing-the-librarys-functionality

--- cap12/minigrep/src/lib.rs
+++ cap12/minigrep/src/lib.rs
@@ -20,6 +20,34 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{content}");
+    for line in search(&config.query, &content) {
+        println!("{line}");
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line.trim());
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, content));
+    }
 }
